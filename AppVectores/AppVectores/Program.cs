65f86bfa37a2d8ca59411498b3cb9727030586af
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-11-22 09:45:23 -0500

    ejercicio

--- AppVectores/AppVectores/Program.cs
+++ AppVectores/AppVectores/Program.cs
@@ -11,7 +11,32 @@ namespace AppVectores
         static void Main(string[] args)
         {
             Console.WriteLine("Control de Versiones");
-            Console.ReadKey();
+            Console.WriteLine(potencia(5, 2));
+            Console.ReadKey(); 
         }
+
+
+        public static double potencia(double x, double y)
+        {
+            if (x <= 0)
+            {
+                throw new Exception("x debe ser positivo");
+            }
+            if(y<=0)
+            {
+                throw new Exception("y debe ser positivo");
+            }
+            double p = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                p = p * x;
+            }
+            return p;
+        }
+
+        
     }
 }
+
+
+
